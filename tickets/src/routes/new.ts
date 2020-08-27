7dded4a71e4a9fd27c38899eac905b7255d1f22c
costanzopa <costanzopa@gmail.com>
2020-08-26 23:59:22 -0300

    Adding tickets endpoints

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response, Router } from 'express';
+import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 import { requireAuth, validateRequest } from '@i-root/common';
 import { Ticket } from '../models/ticket';
@@ -6,27 +6,27 @@ import { Ticket } from '../models/ticket';
 const router = express.Router();
 
 router.post(
-    '/api/tickets',
-    requireAuth,
-    [
-        body('title').not().isEmpty().withMessage('Title is required'),
-        body('price')
-            .isFloat({ gt: 0 })
-            .withMessage('Price must be greater than 0'),
-    ],
-    validateRequest,
-    async (req: Request, res: Response) => {
-        const { title, price } = req.body;
+  '/api/tickets',
+  requireAuth,
+  [
+    body('title').not().isEmpty().withMessage('Title is required'),
+    body('price')
+      .isFloat({ gt: 0 })
+      .withMessage('Price must be greater than 0'),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { title, price } = req.body;
 
-        const ticket = Ticket.build({
-            title,
-            price,
-            userId: req.currentUser!.id,
-        });
-        await ticket.save();
+    const ticket = Ticket.build({
+      title,
+      price,
+      userId: req.currentUser!.id,
+    });
+    await ticket.save();
 
-        res.status(201).send(ticket);
-    }
+    res.status(201).send(ticket);
+  }
 );
 
 export { router as createTicketRouter };
