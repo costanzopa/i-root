e90d75676f700e021fecd949e079357b9174a8c4
costanzopa <costanzopa@gmail.com>
2020-09-10 16:59:55 -0300

    Publishing orders envents

---
+++ orders/src/events/publishers/order-cancelled-publisher.ts
@@ -0,0 +1,5 @@
+import { Subjects, Publisher, OrderCancelledEvent } from '@i-root/common';
+
+export class OrderCancelledPublisher extends Publisher<OrderCancelledEvent> {
+    subject: Subjects.OrderCancelled = Subjects.OrderCancelled;
+}
