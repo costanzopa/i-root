49d4849f2c63eb4c99393dcc57566629313549ac
costanzopa <costanzopa@gmail.com>
2020-09-17 00:17:25 -0300

    Creating Listeners Tests for orders

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -2,11 +2,12 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
-import { errorHandler, NotFoundError, currentUser } from '@sgtickets/common';
-import { createTicketRouter } from './routes/new';
-import { showTicketRouter } from './routes/show';
-import { indexTicketRouter } from './routes/index';
-import { updateTicketRouter } from './routes/update';
+import { errorHandler, NotFoundError } from '@i-root/common';
+
+import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
 
 const app = express();
 app.set('trust proxy', true);
@@ -17,12 +18,11 @@ app.use(
     secure: process.env.NODE_ENV !== 'test',
   })
 );
-app.use(currentUser);
 
-app.use(createTicketRouter);
-app.use(showTicketRouter);
-app.use(indexTicketRouter);
-app.use(updateTicketRouter);
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
 
 app.all('*', async (req, res) => {
   throw new NotFoundError();
